7836502a92bf78b17b6f45dcf2f338dc588ad2ab
BentleyZ0 <BentlyZ0@grangeinsurance.com>
2022-01-21 13:15:31 -0500

    1/20/2022 Challenges

--- 2022/Completed/cc_bentley_2022_1_20a.rb
+++ 2022/Completed/cc_bentley_2022_1_20a.rb
@@ -6,23 +6,20 @@
 # function that will return a hash that shows how much a person needs to spend on
 # needs, wants, and savings.
 # Examples
-# fifty_thirty_twenty(10000) ➞ { "Needs"=> 5000, "Wants"=> 3000,
-# "Savings"=> 2000 }
-# fifty_thirty_twenty(50000) ➞ { "Needs"=> 25000, "Wants"=> 15000,
-# "Savings"=> 10000 }
-# fifty_thirty_twenty(13450) ➞ { "Needs"=> 6725, "Wants"=> 4035,
-# "Savings"=> 2690 }
+# fifty_thirty_twenty(10000) ➞ { "Needs"=> 5000, "Wants"=> 3000, "Savings"=> 2000 }
+# fifty_thirty_twenty(50000) ➞ { "Needs"=> 25000, "Wants"=> 15000, "Savings"=> 10000 }
+# fifty_thirty_twenty(13450) ➞ { "Needs"=> 6725, "Wants"=> 4035, "Savings"=> 2690 }
 
 def fifty_thirty_twenty(ati)
-  { "Needs" => ati*0.50, "Wants" => ati*0.30, "Savings" => ati*0.20 }
+  { "Needs" => ati * 0.50, "Wants" => ati * 0.30, "Savings" => ati * 0.20 }
 end
 
-puts fifty_thirty_twenty(10000) == {"Needs"=> 5000, "Wants"=> 3000, "Savings"=> 2000}
-puts fifty_thirty_twenty(50000) == {"Needs"=> 25000, "Wants"=> 15000, "Savings"=> 10000}
-puts fifty_thirty_twenty(13450) == {"Needs"=> 6725, "Wants"=> 4035, "Savings"=> 2690}
-puts fifty_thirty_twenty(27800) == {"Needs"=> 13900, "Wants"=> 8340, "Savings"=> 5560}
-puts fifty_thirty_twenty(19000) == {"Needs"=> 9500, "Wants"=> 5700, "Savings"=> 3800}
-puts fifty_thirty_twenty(17610) == {"Needs"=> 8805, "Wants"=> 5283, "Savings"=> 3522}
-puts fifty_thirty_twenty(25000) == {"Needs"=> 12500, "Wants"=> 7500, "Savings"=> 5000}
-puts fifty_thirty_twenty(100000) == {"Needs"=> 50000, "Wants"=> 30000, "Savings"=> 20000}
-puts fifty_thirty_twenty(347100) == {"Needs"=> 173550, "Wants"=> 104130, "Savings"=> 69420}
+puts fifty_thirty_twenty(10000) == {"Needs" => 5000, "Wants" => 3000, "Savings" => 2000}
+puts fifty_thirty_twenty(50000) == {"Needs" => 25000, "Wants" => 15000, "Savings" => 10000}
+puts fifty_thirty_twenty(13450) == {"Needs" => 6725, "Wants" => 4035, "Savings" => 2690}
+puts fifty_thirty_twenty(27800) == {"Needs" => 13900, "Wants" => 8340, "Savings" => 5560}
+puts fifty_thirty_twenty(19000) == {"Needs" => 9500, "Wants" => 5700, "Savings" => 3800}
+puts fifty_thirty_twenty(17610) == {"Needs" => 8805, "Wants" => 5283, "Savings" => 3522}
+puts fifty_thirty_twenty(25000) == {"Needs" => 12500, "Wants" => 7500, "Savings" => 5000}
+puts fifty_thirty_twenty(100000) == {"Needs" => 50000, "Wants" => 30000, "Savings" => 20000}
+puts fifty_thirty_twenty(347100) == {"Needs" => 173550, "Wants" => 104130, "Savings" => 69420}
