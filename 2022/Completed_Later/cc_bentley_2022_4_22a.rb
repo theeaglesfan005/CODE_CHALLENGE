b191b0daca214dfbfae2280869c80f19f5501513
BentleyZ0 <BentlyZ0@grangeinsurance.com>
2022-04-28 17:20:15 -0400

    4/22/2022 Code Challenges

---
+++ 2022/Completed_Later/cc_bentley_2022_4_22a.rb
@@ -0,0 +1,22 @@
+# Medium - Check if the String is a Palindrome
+#
+# A palindrome is a word, phrase, number or other sequence of characters which reads the same backward or forward, such as madam or kayak.
+#
+# Write a function that takes a string and determines whether it's a palindrome or not. The function should return a boolean (true or false value).
+#
+# Examples
+# 	- is_palindrome("Neuquen") ➞ true
+# 	- is_palindrome("Not a palindrome") ➞ false
+# 	- is_palindrome("A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!") ➞ true
+#
+# Notes
+# 	- Should be case insensitive.
+# 	- Special characters (punctuation or spaces) should be ignored.
+
+def is_palindrome(str)
+  str.downcase.gsub(/[^a-z]/, '') == str.downcase.gsub(/[^a-z]/, '').reverse
+end
+
+puts is_palindrome("A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!") == true
+puts is_palindrome("Neuquen") == true
+puts is_palindrome("Not a palindrome") == false
---
+++ 2022/Completed_Later/cc_bentley_2022_4_22b.rb
@@ -0,0 +1,27 @@
+# Medium - Squares and Cubes
+#
+# Create a function that takes a list of two numbers and checks if the square root of the first number is equal to the cube root of the second number.
+#
+# Examples
+# 	- check_square_and_cube([4, 8]) ➞ true
+# 	- check_square_and_cube([16, 48]) ➞ false
+# 	- check_square_and_cube([9, 27]) ➞ true
+#
+# Notes
+# 	- Remember to return either true or false.
+# 	- All lists contain two positive numbers.
+
+def check_square_and_cube(lst)
+  Math.sqrt(lst[0]) == Math.cbrt(lst[1])
+end
+
+puts check_square_and_cube([4, 8]) == true
+puts check_square_and_cube([5, 12]) == false
+puts check_square_and_cube([9, 27]) == true
+puts check_square_and_cube([25, 120]) == false
+puts check_square_and_cube([25, 125]) == true
+puts check_square_and_cube([36, 215]) == false
+puts check_square_and_cube([36, 217]) == false
+puts check_square_and_cube([144, 1728]) == true
+puts check_square_and_cube([1, 1]) == true
+puts check_square_and_cube([676, 17576]) == true
