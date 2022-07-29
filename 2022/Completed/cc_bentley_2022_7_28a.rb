7f0a3fac5c616503ec1e7374fefb36f659d1bb6b
theeaglesfan005 <102904&3D10s>
2022-07-29 14:06:13 -0400

    7/28/2022 Code Challenges

---
+++ 2022/Completed/cc_bentley_2022_7_28a.rb
@@ -0,0 +1,66 @@
+# Medium - Book Shelf
+#
+# Create a Book class that has two accessors:
+# 	- title
+# 	- author
+#
+# and two additional methods:
+# 	- A method named get_title that returns: "Title: " + the instance title.
+# 	- A method named get_author that returns: "Author: " + the instance author.
+#
+# and instantiate this constructor by creating 3 new books:
+# 	- Pride and Prejudice - Jane Austen (pp)
+# 	- Hamlet - William Shakespeare (h)
+# 	- War and Peace - Leo Tolstoy (wp)
+# The name of the new hash instances pp, h, and wp, respectively.
+#
+# For instance, if I instantiated the following book:
+# Harry Potter - J.K. Rowling (hp)
+#
+# I would get the following properties and methods:
+# Examples
+# 	- hp.title ➞ "Harry Potter"
+# 	- hp.author ➞ "J.K. Rowling"
+# 	- hp.get_title ➞ "Title: Harry Potter"
+# 	- hp.get_author ➞ "Author: J.K. Rowling"
+# Notes
+# Remember, after you've finished writing the class, you must instantiate it through the creation of new instances.
+
+class Book
+  def title()
+    @title
+  end
+
+  def author()
+    @author
+  end
+
+  def get_title
+    "Title: #{@title}"
+  end
+
+  def get_author
+    "Author: #{@author}"
+  end
+
+  def initialize
+    @title = 'Pride and Prejudice'
+    @author = 'Jane Austen'
+  end
+end
+
+pp = Book.new
+puts pp.title == "Pride and Prejudice"
+puts pp.author == "Jane Austen"
+puts pp.get_title == 'Title: Pride and Prejudice'
+puts pp.get_author == "Author: Jane Austen"
+# #
+# puts h.title == "Hamlet"
+# puts h.author == "William Shakespeare"
+# puts h.get_title == "Title: Hamlet"
+# puts h.get_author == "Author: William Shakespeare"
+#
+# puts wp.title == "War and Peace"
+# puts wp.author == "Leo Tolstoy"
+# puts wp.get_title == "Title: War and Peace"
+# puts wp.get_author == "Author: Leo Tolstoy"
---
+++ 2022/Completed/cc_bentley_2022_7_28b.rb
@@ -0,0 +1,32 @@
+# Medium - Sum of Resistance in Series Circuits
+#
+# When resistors are connected together in series, the same current passes through each resistor in the chain and the
+# total resistance, RT, of the circuit must be equal to the sum of all the individual resistors added together. That is:
+# RT = R1 + R2 + R3 ...
+#
+# Create a function that takes an array of values resistance that are connected in series, and calculates the total
+# resistance of the circuit in ohms. The ohm is the standard unit of electrical resistance in the International System of Units ( SI ).
+#
+# Examples
+# 	- series_resistance([1, 5, 6, 3]) ➞ "15 ohms"
+# 	- series_resistance([16, 3.5, 6]) ➞ "25.5 ohms"
+# 	- series_resistance([0.5, 0.5]) ➞ "1.0 ohm"
+#
+# Notes
+# 	- All inputs will be valid.
+# 	- Notice the singular ohm for values <= 1.
+
+def series_resistance(arr)
+  a = arr.sum
+  a <= 1.0 ? "#{a} ohm" : "#{a} ohms"
+end
+
+puts series_resistance([1, 5, 6, 3]) == "15 ohms"
+puts series_resistance([0.2, 0.3, 0.4]) == "0.9 ohm"
+puts series_resistance([10,12, 1, 10]) == "33 ohms"
+puts series_resistance([10,13, 3.8, 20, 10]) == "56.8 ohms"
+puts series_resistance([0.5, 0.5]) == "1.0 ohm"
+puts series_resistance([16, 30, 22.8, 4]) == "72.8 ohms"
+puts series_resistance([20, 15, 32.5, 2]) == "69.5 ohms"
+puts series_resistance([52, 22, 20, 30]) == "124 ohms"
+puts series_resistance([10, 12, 32, 4.9, 5, 6, 71]) == "140.9 ohms"
